f31085e8b985931f5a69f529cd9f545eb02880d9
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-03-06 11:51:46 -0500

    custom Friendship model

--- src/Models/Friendship.php
+++ src/Models/Friendship.php
@@ -15,6 +15,7 @@ class Friendship extends Model
      * @var array
      */
     protected $guarded = ['id', 'created_at', 'updated_at'];
+    protected $with = ['recipient', 'sender'];
 
     /**
      * @param  array  $attributes
